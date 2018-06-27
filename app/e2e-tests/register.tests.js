426d26426d82bb295457033fed8e834547e1d119
Aaron Schmetzer <as293@hdm-stuttgart.de>
2018-06-27 13:48:10 +0200

    Minor Bugfix Uitest

--- app/e2e-tests/register.tests.js
+++ app/e2e-tests/register.tests.js
@@ -9,12 +9,36 @@ describe('Clicking on the create account button ', function(){
       createAccountButton = element(by.id('createAccountButton'));
     });
 
+
     // deleting jwt token
     afterEach(function() {
       browser.executeScript('window.sessionStorage.clear();');
       browser.executeScript('window.localStorage.clear();');
     });
 
+    it('should successfully sign up and show a pop up saying "Account created"', function() {
+      createAccountButton.click();
+      browser.sleep(500);
+
+      username = element(by.id('loginItemR'));
+      password = element(by.id('passwordItemR'));
+
+      var input = username.element(by.css('input'));
+      browser.actions().mouseMove(input).click();
+      input.sendKeys("admin");
+
+      var input2 = password.element(by.css('input'));
+      browser.actions().mouseMove(input2).click();
+      input2.sendKeys("adminadmin");
+
+      registerButton = element(by.id('registerButton'));
+
+      registerButton.click().then(function() {
+        browser.sleep(500);
+        var popup = element(by.css('.alert-sub-title'));
+        expect(popup.getText()).toEqual("Account created.");
+      });
+    })
     // creating a new account with pseodo random username and password of length 8
     it('should successfully sign up and show a pop up saying "Account created"', function() {
       createAccountButton.click();
